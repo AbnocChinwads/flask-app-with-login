57e768357edd6b27628cd970b8db3b297190da0e
AbnocChinwads <rnemo@hotmail.co.uk>
2020-06-06 18:03:27 +0000

    Removed erroneous code, renamed script.js to darkMode.js

--- static/js/darkMode.js
+++ static/js/darkMode.js
@@ -4,9 +4,6 @@ if(darkMode) {
     $("#darkModeSwitch").prop("checked", true);
     $("body").toggleClass("body-dark");
     $("nav").toggleClass("navbar-dark bg-dark");
-    /*$("label").text(function(i, text){
-        return text === "Dark Mode" ? "Light Mode" : "Dark Mode";
-    })*/
     $("footer").toggleClass("footer-dark");
     $("a").toggleClass("anchor-styling-dark");
 }
@@ -18,9 +15,6 @@ $("#darkModeSwitch").change(function() {
 
     $("body").toggleClass("body-dark", darkMode);
     $("nav").toggleClass("navbar-dark bg-dark", darkMode);
-    /*$("label").text(function(i, text){
-        return text === "Dark Mode" ? "Light Mode" : "Dark Mode";
-    })*/
     $("footer").toggleClass("footer-dark", darkMode);
     $("a").toggleClass("anchor-styling-dark", darkMode);
 });
